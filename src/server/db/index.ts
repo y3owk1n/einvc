d7524efc06d890f2f59a907821831a3363bdb8d6
Kyle Wong <wongyeowkin@gmail.com>
2023-11-26 01:08:42 +0800

    chore: switch drizzle orm to mysql2

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,9 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-
 import { env } from "@/env";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema },
-);
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection(env.DATABASE_URL);
+
+export const db = drizzle(connection, { schema, mode: "planetscale" });
